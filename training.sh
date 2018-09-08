fcb21f823f9b03387f57e2dacc54819bc8295942
Ian Miell <ian.miell@gmail.com>
2018-09-08 07:46:27 +0100

    startagain

--- training.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-bash ./destroy_vms.sh
-asciinema rec -t 'Learn Kubernetes The Hard Way On Vagrant Training' -w 2 -c 'shutit build -d bash -m shutit-library/vagrant -m shutit-library/virtualbox --training' training_video.json
